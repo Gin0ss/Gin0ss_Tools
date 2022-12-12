d54f35634feda4c25fe015eb823e9f6724bd67d4
Gin0ss <115947087+Gin0ss@users.noreply.github.com>
2022-12-12 18:18:47 +0000

    Added Timer
    Timer Functions

--- Vector Maths Tool/Vector Maths Tool/Vector_Form.Designer.cs
+++ Vector Maths Tool/Vector Maths Tool/Vector_Form.Designer.cs
@@ -34,6 +34,8 @@ namespace Vector_Maths_Tool
             this.HeaderText_1 = new System.Windows.Forms.Label();
             this.TimerButton = new System.Windows.Forms.Button();
             this.timerWorker = new System.ComponentModel.BackgroundWorker();
+            this.CreateVector = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // ExitButton
@@ -85,6 +87,32 @@ namespace Vector_Maths_Tool
             this.timerWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.timerWorker_DoWork);
             this.timerWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.timerWorker_ProgressChanged);
             // 
+            // CreateVector
+            // 
+            this.CreateVector.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(16)))), ((int)(((byte)(16)))));
+            this.CreateVector.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.CreateVector.Font = new System.Drawing.Font("Input", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CreateVector.Location = new System.Drawing.Point(1655, 72);
+            this.CreateVector.Name = "CreateVector";
+            this.CreateVector.Size = new System.Drawing.Size(253, 39);
+            this.CreateVector.TabIndex = 3;
+            this.CreateVector.Text = "Create Vector";
+            this.CreateVector.UseVisualStyleBackColor = false;
+            this.CreateVector.Click += new System.EventHandler(this.CreateVector_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(16)))), ((int)(((byte)(16)))));
+            this.ClearButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ClearButton.Font = new System.Drawing.Font("Input", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClearButton.Location = new System.Drawing.Point(1655, 132);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(253, 39);
+            this.ClearButton.TabIndex = 4;
+            this.ClearButton.Text = "Clear Screen";
+            this.ClearButton.UseVisualStyleBackColor = false;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
             // Vector_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 10F);
@@ -92,6 +120,8 @@ namespace Vector_Maths_Tool
             this.AutoSize = true;
             this.BackColor = System.Drawing.SystemColors.Desktop;
             this.ClientSize = new System.Drawing.Size(1920, 1080);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.CreateVector);
             this.Controls.Add(this.TimerButton);
             this.Controls.Add(this.HeaderText_1);
             this.Controls.Add(this.ExitButton);
@@ -104,6 +134,7 @@ namespace Vector_Maths_Tool
             this.Name = "Vector_Form";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Vector Tool";
+            this.Click += new System.EventHandler(this.Vector_Form_Click);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Vector_Graphics_Paint);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -116,6 +147,8 @@ namespace Vector_Maths_Tool
         private System.Windows.Forms.Label HeaderText_1;
         private System.Windows.Forms.Button TimerButton;
         private System.ComponentModel.BackgroundWorker timerWorker;
+        private System.Windows.Forms.Button CreateVector;
+        private System.Windows.Forms.Button ClearButton;
     }
 }
 
--- Vector Maths Tool/Vector Maths Tool/Vector_Form.cs
+++ Vector Maths Tool/Vector Maths Tool/Vector_Form.cs
@@ -2,11 +2,14 @@
 using System.Drawing;
 using System.ComponentModel;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace Vector_Maths_Tool
 {
     public partial class Vector_Form : Form
-    { 
+    {
+        #region Variables
+
         Graphics graphics;
         Brush drawBrush;
         Pen drawPen;
@@ -17,12 +20,22 @@ namespace Vector_Maths_Tool
         Point screenCentre;
         Size drawSize;
 
+        Stopwatch runTimer = new Stopwatch();
+
         bool timerRunning = false;
+        bool isDrawing = false;
+        string timerOut;
+
+        #endregion
+
         public Vector_Form()
         {
+            runTimer.Start();
             InitializeComponent();
+
         }
 
+        #region Graphics
         private void Vector_Graphics_Paint(object sender, PaintEventArgs e)
         {
             graphics = e.Graphics;
@@ -42,17 +55,37 @@ namespace Vector_Maths_Tool
         public void UpdateLine()
         {
             ClearScreen();
+            Cursor.Position = screenCentre;
+            isDrawing = true;
+            currentColor = Color.FromArgb(128, 12, 220, 12);
             graphics.DrawLine(drawPen, new Point(MousePosition.X - windowCentre.X, MousePosition.Y - windowCentre.Y), windowCentre);
 
         }
 
-        public void ClearScreen()
+        public void ClearScreen(PaintEventArgs e)
         {
             graphics.Clear(BackColor);
-            graphics.FillEllipse(drawBrush, new Rectangle(windowCentre, drawSize));
+            //Other graphical constants
 
         }
 
+        #endregion
+
+        #region User_Input
+        private void Vector_Form_Click(object sender, EventArgs e)
+        {
+            if (isDrawing)
+            {
+                currentColor = Color.FromArgb(255, 16, 255, 8);
+                graphics.DrawLine(drawPen, new Point(MousePosition.X - windowCentre.X, MousePosition.Y - windowCentre.Y), windowCentre);
+                isDrawing = false;
+            }
+        }
+
+        #endregion
+
+        #region Buttons
+        //Closes App
         private void ExitButton_Click(object sender, EventArgs e)
         {
             if (timerWorker.WorkerSupportsCancellation) { timerWorker.CancelAsync(); }
@@ -60,11 +93,13 @@ namespace Vector_Maths_Tool
 
         }
 
+        //Title text on click shows info
         private void HeaderText_1_Click(object sender, EventArgs e)
         {
-            Console.WriteLine("Vector Tools by Gin0ss || V. 0.01");
+            Console.WriteLine("Vector Tools by Gin0ss || V. 0.03");
         }
 
+        #region Timer
         private void TimerButton_Click(object sender, EventArgs e)
         {
             if (timerRunning) { timerRunning = false; }
@@ -73,24 +108,59 @@ namespace Vector_Maths_Tool
 
         }
 
+        //Timer Button Async Thread
         private void timerWorker_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
         {
-                BackgroundWorker worker = sender as BackgroundWorker;
+            BackgroundWorker worker = sender as BackgroundWorker;
 
-                int time = 0;
-                while (timerRunning)
-                {
-                    System.Threading.Thread.Sleep(1);
-                    time+=1;
-                    worker.ReportProgress(time);
+            int ms = 0;
+            int seconds = 0;
+            int mins = 0;
+            int hours = 0;
 
-                }
+            while (timerRunning)
+            {
+                System.Threading.Thread.Sleep(10);
+                ms++;
+                timerOut = Convert.ToString(seconds);
+
+                timerOut = hours + ":" + ConvertTime(ref mins, ref hours, 60) + ":" + ConvertTime(ref seconds, ref mins, 60) + "." + ConvertTime(ref ms, ref seconds, 100);
+                worker.ReportProgress(seconds);
+
+            }
 
         }
 
+        //Converts Time unit to another in string
+        string ConvertTime(ref int currentInt, ref int newInt, int maxValue)
+        {
+            if (currentInt >= maxValue)
+            {
+                currentInt = 0;
+                newInt++;
+                return Convert.ToString(newInt);
+            }
+            else { return Convert.ToString(currentInt); }
+
+        }
+
+        //Async thread reported to main thread for UI to process
         private void timerWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
-            TimerButton.Text = e.ProgressPercentage.ToString();
+            TimerButton.Text = timerOut;
+        }
+        #endregion
+
+        #endregion
+
+        private void CreateVector_Click(object sender, EventArgs e)
+        {
+            UpdateLine();
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            ClearScreen();
         }
 
     }
